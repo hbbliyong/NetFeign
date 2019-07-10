a6c2fd7e40226d3962f89dd9f3375e27946b1f29
hbbliyong <hbbliyong@163.com>
2019-07-10 15:54:04 +0800

    上传代理工厂

---
+++ NetFeign/ServiceProxyFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFeign
+{
+    public class ServiceProxyFactory<T>
+    {
+        public static T Create()
+        {
+            FeignRealProxy<T> realProxy = new FeignRealProxy<T>();
+            return (T)realProxy.GetTransparentProxy();
+        }
+    }
+}
